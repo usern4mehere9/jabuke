655089f9bf35ff49dc4efe2d925ee27d7109490b
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-10 13:33:30 +0200

    Add files via upload

---
+++ jabuke/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace jabuke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p, m;
+            p = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+
+            if (p > m)
+            {
+                Console.WriteLine("DA");
+            }
+            else
+            {
+                Console.WriteLine("NE");
+            }
+        }
+    }
+}
